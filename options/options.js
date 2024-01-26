07c2f5b1a9742368bff53dfb30887df4c3928f8e
Crowdbotics <team@crowdbotics.com>
2024-01-26 20:00:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "alliedbarton-guardi-45596",
+  url: "https://alliedbarton-guardi-45596.botics.co",
+  api: "https://alliedbarton-guardi-45596.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
